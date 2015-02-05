8d9ff7fc48e44d21db772cf6886e68ae7a3e0951
Christian Leger <christian@boldradius.com>
2015-02-05 10:31:58 -0500

    Added a few extra routes and controllers demonstrating when i18n does or doesn't use the useragent-supplied preferred language.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,9 +9,20 @@ import play.api.Play.current
 
 object Application extends Controller {
 
-  // This will always use the default language of the application
+  // This will always use the default language of the application - user settings ignored!
   def alwaysDefault = Action {
     Ok(Messages("hello"))
+  }
+
+  // This will always apply localization as requested by the client agent.
+  def alwaysDefaultWithRequest = Action { request =>
+    Ok(Messages("hello"))
+  }
+
+  // This will ignore the language supplied to withLang! Compare with next one,
+  // which uses the user-chosen language. The difference is that the request parameter here is not implicit. 
+  def explicitLanguageIgnored = Action { request => 
+    Ok(Messages("hello")).withLang(request.acceptLanguages.head)
   }
 
   // This will apply the first localization present in the client's Accept-language header component. 
@@ -24,14 +35,9 @@ object Application extends Controller {
     Ok(Messages("hello")(request.acceptLanguages.head))
   }
 
-  // This will ignore the language supplied to withLang! Because the request object is not made implicit. 
-  def explicitLanguageIgnored = Action { request => 
-    Ok(Messages("hello")).withLang(request.acceptLanguages.head)
-  }
-
-  // This will always apply localization as requested by the client agent.
-  def alwaysDefaultWithRequest = Action { request =>
-    Ok(Messages("hello"))
+  // This will apply the first localization present in the client's Accept-language header component. 
+  def explicitLanguageV3 = Action { request => 
+    Ok(Messages("hello")(request.acceptLanguages.head))
   }
 
   // This will always apply localization as requested by the client agent. 
